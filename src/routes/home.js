966fca6eb87a68d3f97c094bcaa4fe2f99d59071
Boban Alempijevic <boban.alempijevic@gmail.com>
2020-04-05 10:44:59 +0300

    Added two boxes for demand and supply

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,75 @@
 import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-const Home = () => (
-  <Container>
-    <Typography>
-      Home route
-    </Typography>
-  </Container>
-);
+const useStyles = makeStyles({
+  root: {
+    minWidth: 250,
+    flexGrow: 1,
+  },
 
-export default Home;
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  pad: {
+    padding: 10,
+  },
+});
+
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Card className={classes.root} >
+            <CardContent>
+
+              <Typography variant="h5" component="h2" className={classes.pad}>
+                Demand
+              </Typography>
+
+              <Typography variant="body2" component="p" className={classes.pad}>
+                We are in need of supply
+              </Typography>
+
+            </CardContent>
+            <CardActions>
+              <Button size="large" variant="contained" color="primary">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Card className={classes.root} >
+            <CardContent>
+
+              <Typography variant="h5" component="h2" className={classes.pad}>
+                Supply
+              </Typography>
+
+              <Typography variant="body2" component="p" className={classes.pad}>
+                We can supply you
+              </Typography>
+
+            </CardContent>
+            <CardActions>
+              <Button size="large" variant="contained" color="primary">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+      </Grid>
+    </Container>
+  );
+}
