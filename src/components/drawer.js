e4473c54f0a10fabe25eb525984e1c1bced486a3
Matias Marti <matias97marti@gmail.com>
2020-04-05 06:31:55 +0000

    :boom: fix register logic and create drawer

---
+++ src/components/drawer.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3)
+  }
+}));
+
+export default function PermanentDrawerLeft() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar} />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {["login", "register", "home", "Route"].map(text => (
+            <ListItem button key={text}>
+              <NavLink to={`/${text}`}>{text}</NavLink>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
+}
--- src/routes/register.js
+++ src/routes/register.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import React, { useState } from "react";
+import { Container, TextField, Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import request from "../request";
 
-const Register = () => (
-  <Container>
-    <Typography>
-      Register route
-    </Typography>
-  </Container>
-);
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const history = useHistory();
+
+  const submitForm = async () => {
+    if (!email && !password) return alert("Missing email or password!");
+
+    try {
+      const response = await request("Auth", "/api", "POST", {
+        email,
+        password
+      });
+      console.log(response);
+      history.push("/login");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Container>
+      <center className="flex flex-col rounded border-1 border-black">
+        <TextField
+          onChange={e => setEmail(e.target.value)}
+          required
+          type="email"
+          id="standard-basic"
+          label="email"
+          value={email}
+        />
+        <TextField
+          required
+          value={password}
+          placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          name="password"
+        />
+        <Button onClick={submitForm} variant="contained" color="secondary">
+          Submit
+        </Button>
+      </center>
+    </Container>
+  );
+};
 
 export default Register;
