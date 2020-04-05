0d5aedc50746cb09d1c96b8a4394df67c059e3de
Marcelo Iepsen <iepsen@gmail.com>
2020-04-05 12:24:02 +0200

    Add styles, moved components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
-import Home from "./home";
-import Register from "./register";
-import Login from "./login";
-import Search from "./search";
+import Home from './home';
+import Register from './register';
+import Login from './login';
+import Search from './search';
+import { AddDemand, FeedbackDemand } from './demand';
 
-export { Home, Register, Login, Search };
+export {
+  Home, Register, Login, Search, AddDemand, FeedbackDemand,
+};
