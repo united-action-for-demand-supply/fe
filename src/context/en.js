8cee17985978056443a796fc0500aecd159189e3
Marcelo Iepsen <iepsen@gmail.com>
2020-04-05 09:57:26 +0200

    Add language text

--- src/context/en.js
+++ src/context/en.js
@@ -1,5 +1,12 @@
 const en = {
   title: 'Covid-19 🦠 Goods logistics for supply-and-demand and logistics orchestrator.',
+  home: {
+    demandTitle: 'Demand',
+    demandDescription: 'We are in need of supply',
+    supplyTitle: 'Supply',
+    supplyDescription: 'We can supply you',
+    learnMore: 'Learn More',
+  },
 };
 
 export default en;
--- src/context/sv.js
+++ src/context/sv.js
@@ -1,5 +1,12 @@
-const sv = {
-  title: 'Swedish text',
+const en = {
+  title: 'Covid-19 🦠 Goods logistics for supply-and-demand and logistics orchestrator.',
+  home: {
+    demandTitle: 'Demand (replace with SV)',
+    demandDescription: 'We are in need of supply (replace with SV)',
+    supplyTitle: 'Supply (replace with SV)',
+    supplyDescription: 'We can supply you (replace with SV)',
+    learnMore: 'Learn More (replace with SV)',
+  },
 };
 
-export default sv;
+export default en;
