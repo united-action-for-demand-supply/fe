246b2894c467c4afbfd9c648ef3660299d519276
Marcelo Iepsen <iepsen@gmail.com>
2020-04-05 09:44:29 +0200

    Use Context API

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import {
   HashRouter, Switch, Route,
 } from 'react-router-dom';
+import { LanguageProvider } from './context/language';
 import { Home, Register, Login } from './routes';
 
 const App = () => (
-  <HashRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-    </Switch>
-  </HashRouter>
+  <LanguageProvider>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </HashRouter>
+  </LanguageProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
